f34cf787c99acda9dace737ccee7bc8d91851edb
pavelkemko <pavelkemko@gmail.com>
2023-01-09 01:56:23 +0200

    feat: done project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
-}
+  let absoluteNumber = Math.abs(n);
+  let reverseString = absoluteNumber.toString().split("").reverse().join("");
+  return +reverseString;
+};
